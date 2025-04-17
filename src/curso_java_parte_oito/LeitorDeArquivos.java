572b32197494ada2bea455439d370f0df9bd74c0
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-04-17 13:23:42 -0300

    scanner como leitor de arquivos

---
+++ src/curso_java_parte_oito/LeitorDeArquivos.java
@@ -0,0 +1,29 @@
+package curso_java_parte_oito;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LeitorDeArquivos {
+
+	public static void main(String[] args) {
+
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+
+		try {
+			sc = new Scanner(file); // instancia-se o scanner com o próprio arquivo
+			while (sc.hasNextLine()) { // testa se há uma nova linha no arquivo
+				System.out.println(sc.nextLine()); // para imprimir a nova linha caso exista
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+}
