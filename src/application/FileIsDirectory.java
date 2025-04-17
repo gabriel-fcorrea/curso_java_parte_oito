98e716b2b0d167ce9791c30b82d26fec850873fa
gabriel-fcorrea <gabrecorrea@gmail.com>
2025-04-17 15:06:59 -0300

    mapeamento de diretórios e arquivos + criação de subpastas

---
+++ src/application/FileIsDirectory.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileIsDirectory {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String strPath = sc.nextLine();
+
+		File path = new File(strPath);
+
+		// coleta o mapa de diretórios:
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES: ");
+		for (File file : files) {
+			System.out.println(file);
+		}
+
+		// para criar uma subpasta:
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+
+		sc.close();
+	}
+}
